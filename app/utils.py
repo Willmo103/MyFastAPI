30338ed811b1f97ee709b7d1ec96a1bc75977488
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-08-22 03:44:51 -0500

    part 2 authentication created the oauth function to generate the JWT

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,13 @@
 from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 # this is to hash passwords so as not to save them as plain text
 # in the database.
-
-
 def hash(password: str):
     return pwd_context.hash(password)
 
 
-def verify(plain_password, hashed_password):
+def verify(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
