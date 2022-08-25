3e13eed0be47371139fae71235a876e4dfeda359
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-08-25 00:25:58 -0500

    all endpoints mapped to relational auth

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, String, Integer, Boolean, TIMESTAMP, text
+from sqlalchemy import Column, String, Integer, Boolean, TIMESTAMP, text, ForeignKey
 
 
 class Post(Base):
@@ -9,7 +9,9 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="TRUE", nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text("now()"))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 
 class User(Base):
@@ -17,5 +19,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
-
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text("now()"))
--- app/routers/post.py
+++ app/routers/post.py
@@ -11,10 +11,10 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     print(posts)
     return posts
 
@@ -27,8 +27,7 @@ def create_post(post: schemas.PostCreate,
     #                (post.title, post.content, post.published))
     # connection.commit()
     # new_post = cursor.fetchone()
-    print(current_user)
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -52,11 +51,16 @@ def delete_post(id: str, db: Session = Depends(get_db),
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id)))
     # post = cursor.fetchone()
     # connection.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() is None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} no longer exists.")
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -69,11 +73,19 @@ def update_post(id: int, post: schemas.PostCreate,
     #                (post.title, post.content, post.published, str(id)))
     # post = cursor.fetchone()
     # connection.commit()
+
     post_query = db.query(models.Post).filter(models.Post.id == id)
+
     original_post = post_query.first()
+
     if original_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} no longer exists.")
+
+    if original_post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
     post_query.update(post.dict(), synchronize_session=False)
+
     db.commit()
+
     return post_query.first()
