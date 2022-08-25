ad1ba051d64bd5af73bc06ca3b80e68ce904214c
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-08-25 02:04:56 -0500

    added search params to getposts

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,8 +1,8 @@
 from .. import schemas, models, oauth2
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from fastapi import Response, status, HTTPException, Depends, APIRouter
 from ..database import get_db
-from typing import List
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/posts",
@@ -11,10 +11,12 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
+              limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    print(limit)
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
--- app/schemas.py
+++ app/schemas.py
@@ -36,7 +36,7 @@ class PostCreate(PostBase):
 class Post(PostBase):
     id: int
     created_at: datetime
-    owner_id: int
+    # owner_id: int
     owner: UserOut
 
     class Config:
